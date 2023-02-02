9830394c11bc3f063a5f054300c9803bd33c62a2
Russ-S <rstanleym74@gmail.com>
2023-02-02 17:12:16 +0000

    JS: add api info for TMDB

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2994c4147e2afe49f6667536b6aec78&page=1'
+//
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w500'
+
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=2994c4147e2afe49f6667536b6aec78&query="'
