bf05f0a4b0a18a2fb7d27f4fb6c8ff465cc6f6ef
Russ-S <rstanleym74@gmail.com>
2023-02-03 14:15:24 +0000

    JS: setup api and create functions for calling movies

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,40 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2994c4147e2afe49f6667536b6aec78&page=1'
-//
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2994c4147e2afe49f6667536b6aec78e&page=1';
 
-const IMG_PATH = 'https://image.tmdb.org/t/p/w500'
+// take the url + discover example (from the docs) & api key (from account setup) & page 1 for first set of results
 
-const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=2994c4147e2afe49f6667536b6aec78&query="'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+// create the variable for the image path to concatenate with the jpg from the api data.. Change the width if necessary (it defaults to /w500)
+
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=2994c4147e2afe49f6667536b6aec78e&query="'; // same root url as above but change /discover/ to /search/ and remove the sort and add &query=" - note the single double quote - to concatenate the search term from the search box into this variable
+
+const form = document.getElementById('form');
+
+const search = document.getElementById('search');
+
+// Get initial movies ---------------------->
+
+getMovies(API_URL)
+
+async function getMovies(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    console.log(data.results);
+} // makes a request. They return a promise that resolves with the result of the asynchronous operation.  use the await keyword inside an async function to wait for a promise to resolve and get its result. The code inside the async function will pause until the promise is resolved and then continue.
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault(); //listens for the submit event, then prevents the event from submitting it as per the defualt action
+
+    const searchTerm = search.value;
+
+    if (searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm);
+        //if searchTerm exists and is not equal to nothing, then concats the searchTerm to the search api url
+        search.value = '';
+        //this clears the search bar
+    } else {
+        window.location.reload()
+        //if submit is pressed with nothing in the search, then relaod the page
+    }
+
+});
